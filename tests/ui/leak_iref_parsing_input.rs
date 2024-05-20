fb3377453b34ed4a888f49d54a24ba1c5389d933
hatoo <hato2000@gmail.com>
2024-05-20 16:38:01 +0900

    add

---
+++ tests/ui/leak_iref_parsing_input.rs
@@ -0,0 +1,25 @@
+use futures::FutureExt;
+use stap::{Cursor, Input, ParsingInput};
+
+fn main() {
+    let input = Input::new(Cursor {
+        buf: Vec::<u8>::new(),
+        index: 0,
+    });
+
+    let mut leak = None;
+
+    let mut pi = ParsingInput::new(input);
+
+    pi.start_parsing(|iref| {
+        let leak_mut = &mut leak;
+        async move {
+            *leak_mut = Some(iref);
+        }
+        .boxed_local()
+    });
+
+    pi.poll();
+
+    drop(leak);
+}
