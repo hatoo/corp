833c3ed9040284307226406e10082a05fa708b02
hatoo <hato2000@gmail.com>
2024-05-25 15:35:58 +0900

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,15 @@ use std::{
 
 use futures::task::noop_waker_ref;
 
-/// buf and index
+/// Data of input.
+/// Buffer and current index.
 #[derive(Debug)]
 pub struct Cursor<T> {
     /// Sequence of items, you may append items to this when you want.
-    /// I think reducing the items of `buf` isn't introduce an unsoundness (only may panics) but you don't want to do that.
+    /// This crate loosely assumes that you don't remove items from this.
     pub buf: Vec<T>,
-    /// Some of parsers in this crate assumes index <= buf.len().
+    /// Current index of the cursor.
+    /// This crate assumes index <= buf.len().
     pub index: usize,
 }
 
@@ -27,15 +29,19 @@ impl<T> Cursor<T> {
 #[cfg(debug_assertions)]
 #[repr(transparent)]
 #[derive(Debug)]
+/// Just a wrapper of Cursor.
 pub struct Input<T>(std::cell::RefCell<Cursor<T>>);
 
 #[cfg(not(debug_assertions))]
 #[repr(transparent)]
 #[derive(Debug)]
+/// Just a wrapper of Cursor.
+/// This is used as input of parsers.
 pub struct Input<T>(std::cell::UnsafeCell<Cursor<T>>);
 
 impl<T> Input<T> {
     #[inline]
+    /// Create a new Input from Cursor.
     pub fn new(cursor: Cursor<T>) -> Self {
         #[cfg(debug_assertions)]
         {
@@ -48,6 +54,7 @@ impl<T> Input<T> {
     }
 
     #[inline]
+    /// Get a reference of Cursor.
     pub fn cursor(&self) -> impl Deref<Target = Cursor<T>> + '_ {
         #[cfg(debug_assertions)]
         {
@@ -59,6 +66,7 @@ impl<T> Input<T> {
         }
     }
     #[inline]
+    /// Get a mutable reference of Cursor.
     pub fn cursor_mut(&mut self) -> impl DerefMut<Target = Cursor<T>> + '_ {
         #[cfg(debug_assertions)]
         {
@@ -148,6 +156,8 @@ impl<T> Input<T> {
     }
 
     #[inline]
+    /// Start parsing with a parser.
+    /// Just a wrapper of Parsing::new.
     pub fn start_parsing<'a, O, F, P>(&'a mut self, parser: P) -> Parsing<'a, T, O, F>
     where
         P: Parser<'a, T, O, F>,
@@ -157,12 +167,16 @@ impl<T> Input<T> {
     }
 
     #[inline]
+    /// Start parsing with a parser.
+    /// Just a wrapper of ParsingInput::new.
     pub fn into_parsing_input<O, F>(self) -> ParsingInput<T, O, F> {
         ParsingInput::<T, O, F>::new(self)
     }
 }
 
 #[derive(Debug)]
+/// A reference of Cursor for parsers.
+/// This ensures you can't get &mut buf in parsers.
 pub struct CursorRef<'a, T> {
     cursor: &'a mut Cursor<T>,
 }
@@ -170,21 +184,25 @@ pub struct CursorRef<'a, T> {
 // you can't get &mut buf
 impl<'a, T> CursorRef<'a, T> {
     #[inline]
+    /// Get the current index.
     pub fn index(&self) -> usize {
         self.cursor.index
     }
 
     #[inline]
+    /// Get the mutable reference of the current index.
     pub fn index_mut(&mut self) -> &mut usize {
         &mut self.cursor.index
     }
 
     #[inline]
+    /// Utility function. Get the remaining items.
     pub fn remaining(&self) -> &[T] {
         &self.cursor.buf[self.cursor.index..]
     }
 
     #[inline]
+    /// Get the buffer.
     pub fn buf(&self) -> &[T] {
         &self.cursor.buf
     }
@@ -192,9 +210,12 @@ impl<'a, T> CursorRef<'a, T> {
 
 #[repr(transparent)]
 #[derive(Debug)]
+/// A reference of Input.
+/// This is used as input of parsers.
 pub struct InputRef<'a, T>(&'a Input<T>);
 
 impl<'a, T> InputRef<'a, T> {
+    /// Do something with a mutable reference of Cursor.
     /// This crate is safe if you can't bring arguments to outside. I believe it is true.
     #[inline]
     pub fn scope_cursor_mut<O>(&mut self, jail: impl FnOnce(&mut CursorRef<T>) -> O) -> O {
@@ -205,6 +226,7 @@ impl<'a, T> InputRef<'a, T> {
         })
     }
 
+    /// Do something with a reference of Cursor.
     /// This crate is safe if you can't bring arguments to outside. I believe it is true.
     #[inline]
     pub fn scope_cursor<O>(&self, jail: impl FnOnce(&CursorRef<T>) -> O) -> O {
@@ -216,16 +238,19 @@ impl<'a, T> InputRef<'a, T> {
     }
 
     #[inline]
+    /// Stop parsing until the buffer is extended.
     pub fn read(&mut self) -> impl Future<Output = ()> + '_ {
         self.0.read()
     }
 
     #[inline]
+    /// Stop parsing until the buf.len() >= index + at_least.
     pub fn read_n(&mut self, at_least: usize) -> impl Future<Output = ()> + '_ {
         self.0.read_n(at_least)
     }
 }
 
+/// Parser tarit
 pub trait Parser<'a, T, O, F>: FnOnce(InputRef<'a, T>) -> F
 where
     T: 'a,
@@ -242,6 +267,7 @@ where
 }
 
 #[derive(Debug)]
+/// Parsing state holds a reference of Input.
 pub struct Parsing<'a, T, O, F> {
     input: &'a Input<T>,
     result: Option<O>,
@@ -253,6 +279,7 @@ where
     F: Future<Output = O> + Unpin + 'a,
 {
     #[inline]
+    /// Create a new Parsing from Input and a parser.
     pub fn new<P: Parser<'a, T, O, F>>(input: &'a mut Input<T>, parser: P) -> Self {
         // Dupe mutable ref
         Self {
@@ -265,6 +292,9 @@ where
     }
 
     #[inline]
+    /// Run the parser.
+    /// Return true if the parser is done. You shouldn't call this anymore. It may leads to panic or never return.
+    /// Return false if the parser is pending. You should add more items to the buffer.
     pub fn poll(&mut self) -> bool {
         let mut cx = Context::from_waker(noop_waker_ref());
         match Pin::new(&mut self.future).poll(&mut cx) {
@@ -279,22 +309,29 @@ where
 
 impl<'a, T, O, F> Parsing<'a, T, O, F> {
     #[inline]
+    /// Get the reference of Cursor.
     pub fn cursor(&self) -> impl Deref<Target = Cursor<T>> + '_ {
         self.input.cursor()
     }
 
     #[inline]
+    /// Get the mutable reference of Cursor.
+    /// You can add items to the buffer.
     pub fn cursor_mut(&mut self) -> impl DerefMut<Target = Cursor<T>> + '_ {
         unsafe { self.input.cursor_mut_unsafe() }
     }
 
     #[inline]
+    /// Get the result of the parser.
+    /// Return Some(_) if the parser is done (= poll() returned true).
+    /// Return None otherwise.
     pub fn into_result(self) -> Option<O> {
         self.result
     }
 }
 
 #[derive(Debug)]
+/// Parsing state holds an Input.
 pub struct ParsingInput<T, O, F> {
     input: Input<T>,
     result: Option<O>,
@@ -302,6 +339,7 @@ pub struct ParsingInput<T, O, F> {
 }
 
 impl<T, O, F> ParsingInput<T, O, F> {
+    /// Create a new ParsingInput from Input.
     pub fn new(input: Input<T>) -> Self {
         Self {
             input,
@@ -311,21 +349,25 @@ impl<T, O, F> ParsingInput<T, O, F> {
     }
 
     #[inline]
+    /// Get the reference of Cursor.
     pub fn cursor(&self) -> impl Deref<Target = Cursor<T>> + '_ {
         self.input.cursor()
     }
 
     #[inline]
+    /// Get the mutable reference of Cursor.
     pub fn cursor_mut(&mut self) -> impl DerefMut<Target = Cursor<T>> + '_ {
         self.input.cursor_mut()
     }
 
     #[inline]
+    /// Get the result of the parser.
     pub fn result_mut(&mut self) -> Option<&mut O> {
         self.result.as_mut()
     }
 
     #[inline]
+    /// Break the ParsingInput into Input.
     pub fn into_input(self) -> Input<T> {
         self.input
     }
@@ -336,6 +378,7 @@ where
     F: Future<Output = O>,
 {
     #[inline]
+    /// Start parsing with a parser.
     pub fn start_parsing<'a, P: Parser<'a, T, O, F>>(&mut self, parser: P)
     where
         T: 'a,
@@ -347,6 +390,9 @@ where
     }
 
     #[inline]
+    /// Run the parser.
+    /// Return true if the parser is done. You shouldn't call this anymore. It may leads to panic or never return.
+    /// Return false if the parser is pending. You should add more items to the buffer.
     pub fn poll(&mut self) -> bool
     where
         F: Unpin,
@@ -367,6 +413,8 @@ where
     }
 }
 
+/// Anchoring the current index of the cursor.
+/// Restore index when dropped.
 pub struct Anchor<'a, 'b, T> {
     pub iref: &'a mut InputRef<'b, T>,
     pub index: usize,
@@ -374,6 +422,7 @@ pub struct Anchor<'a, 'b, T> {
 
 impl<'a, 'b, T> Anchor<'a, 'b, T> {
     #[inline]
+    /// Create a new Anchor from InputRef.
     pub fn new(iref: &'a mut InputRef<'b, T>) -> Self {
         Self {
             index: iref.scope_cursor(|c| c.index()),
@@ -382,12 +431,15 @@ impl<'a, 'b, T> Anchor<'a, 'b, T> {
     }
 
     #[inline]
+    /// Set the current index to the anchor.
     pub fn renew(&mut self) {
         self.index = self.iref.scope_cursor(|c| c.index());
     }
 
     #[inline]
-    pub fn commit(self) -> &'a mut InputRef<'b, T> {
+    /// Forget the anchor.
+    /// The current index of the cursor is unchanged.
+    pub fn forget(self) -> &'a mut InputRef<'b, T> {
         let m = ManuallyDrop::new(self);
         let iref = unsafe { std::ptr::read(&m.iref) };
 
@@ -420,6 +472,8 @@ impl<'a, 'b, T> DerefMut for Anchor<'a, 'b, T> {
     }
 }
 
+/// Match a single item
+/// Advance the cursor index if the item is matched.
 pub async fn just<T>(input: &mut InputRef<'_, T>, t: T) -> Result<usize, ()>
 where
     T: PartialEq,
@@ -437,6 +491,8 @@ where
     })
 }
 
+/// Match a sequence of items
+/// Advance the cursor index if the sequence is matched.
 pub async fn tag<T>(input: &mut InputRef<'_, T>, tag: &[T]) -> Result<Range<usize>, ()>
 where
     T: PartialEq,
@@ -454,6 +510,9 @@ where
     })
 }
 
+/// Match zero or more items until cond is true.
+/// Return the range of the matched items including zero range.
+/// Advance the cursor index as much as matched.
 pub async fn many0<T>(
     input: &mut InputRef<'_, T>,
     mut cond: impl FnMut(&T) -> bool,
@@ -480,6 +539,9 @@ pub async fn many0<T>(
     }
 }
 
+/// Match one or more items until cond is true.
+/// Return the range of the matched items including or Err for no match at all.
+/// Advance the cursor index as much as matched.
 pub async fn many1<T>(
     input: &mut InputRef<'_, T>,
     mut cond: impl FnMut(&T) -> bool,
@@ -661,7 +723,7 @@ mod tests {
             dbg!(&digit);
             let alpha2 = many1(&mut anchor, |x: &u8| x.is_ascii_alphabetic()).await?;
 
-            anchor.commit();
+            anchor.forget();
 
             Ok((alpha0, digit, alpha2))
         }
